9968cbbba505a9e622fff2916a8b4191eefe273c
Callum Alton <callum.alton33@gmail.com>
2020-05-05 12:11:06 +0100

    added to DOM files; for loops, functions etc.

--- Week 2/DOM/index.js
+++ Week 2/DOM/index.js
@@ -1,35 +1,59 @@
-// alert("Did this work?")
 
 // can manipulate elements and then change the display 
 // can be done with JS aswell as CSS
 
-document.getElementById("heading").style.color = "purple"
-document.getElementById("heading").style.backgroundColor = "pink"
+// style a single variable with multiple attributes, 
+// ie color, 
+// size, etc
 
-const myFirstHeading = document.getElementById("heading")
 const button = document.getElementById("button")
 const input = document.getElementById("input")
-const paragraphs = document.getElementsByTagName("p")
 
 // For loops allow to go through text more quickly 
 
+const paragraphs = document.getElementsByTagName("p")
 for (i = 0; i < 3; i++) {
-paragraphs[i].style.color = "red"
+paragraphs[1].style.color = "red"
 }
 
-// Allows the click of your mouse to chnage colour
-
+// Allows the click of your mouse to change colour
+const myFirstHeading = document.getElementById("heading")
 myFirstHeading.addEventListener("click", () => {
-    myFirstHeading.style.color = "blue" 
+    myFirstHeading.style.color = "purple"
 })
+
+myFirstHeading.style.color = "blue" 
+myFirstHeading.style.backgroundColor = "pink"
+myFirstHeading.style.fontSize = "50px"
+myFirstHeading.style.fontStyle = "italic"
 
 // Adds button element
 
 button.addEventListener("click", () => {
     // myFirstHeading.style.color = "yellow"
     myFirstHeading.style.color = input.value // input color into button element and it changes
+    myFirstHeading.style.fontSize = input.value // inputs font size into button element
+    // user has to input a number then 'px' after
 })
 
+// Targets all small headings (h2)
+
+const smallerHeading = document.getElementsByClassName("smallerHeading")
+for (i = 0; i < smallerHeading.length; i++) {
+    smallerHeading[1].style.color = "pink"
+}
+
+// .querySelectorAll selects all "li"
+
+const list = document.querySelectorAll("li");
+for (i = 0; i < list.length; i++) {
+    list[1].style.color = "orange"
+}
+
+// in a function, 
+// style a single variable with multiple attributes, 
+// ie color, 
+// size, etc
 
 /**
  * three variables have been created
