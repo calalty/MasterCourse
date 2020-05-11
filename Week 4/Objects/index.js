699d66a4e0d962d284d1192ff41c93b55ddee481
Callum Alton <callum.alton33@gmail.com>
2020-05-11 15:55:09 +0100

    week 4

--- Week 4/Objects/index.js
+++ Week 4/Objects/index.js
@@ -22,3 +22,15 @@ console.table(carObjects['doors']) // has to have quotations to access element
 delete carObjects.color // or delete person[hobby];
 
 console.table(carObjects)
+
+for (let [key, value] of Object.entries(carObjects)) {
+    console.log(key)
+    console.log(value)
+    break
+}
+
+mainObject.nextLevelObject
+
+// The Object.entries() method returns an array of a given object's own enumerable string-keyed property [key, value] pairs, 
+// in the same order as that provided by a for...in loop. 
+// (The only important difference is that a for...in loop enumerates properties in the prototype chain as well). 
---
+++ Week 4/for of/index.js
@@ -0,0 +1,17 @@
+let arr = ['a', 'b', 'c']
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+
+// for of is an alternative and reads better
+
+for (const letter of arr) {
+    console.log(letter)
+}
+
+// 
+//
+//
+//
+//
